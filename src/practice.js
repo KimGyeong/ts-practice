630d4b9fd6887b178b0fefd9836eb4e15a9120f5
hwangbo <taekwonod714@naver.com>
2020-07-01 17:14:32 +0900

    feat: 인터페이스 객체를 만들어보자

--- src/practice.js
+++ src/practice.js
@@ -1,27 +1,10 @@
 "use strict";
-// 상속받아 해당 클래스가 Shape interface 의 조건을 충족시킨다.
-var Circle = /** @class */ (function () {
-    /* 생성자 */
-    function Circle(radius) {
-        this.radius = radius;
-    }
-    /* 오버라이드 함수 */
-    Circle.prototype.getArea = function () {
-        return this.radius * this.radius * Math.PI;
-    };
-    return Circle;
-}());
-var Rectangle = /** @class */ (function () {
-    function Rectangle(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-    Rectangle.prototype.getArea = function () {
-        return this.width * this.height;
-    };
-    return Rectangle;
-}());
-var shapes = [new Circle(5), new Rectangle(10, 5)];
-shapes.forEach(function (shape) {
-    console.log(shape.getArea());
-});
+var person = {
+    name: '김사람',
+    age: 20
+};
+var expert = {
+    name: '김개발',
+    skills: ['javascript', 'react']
+};
+var people = [person, expert];
--- src/practice.ts
+++ src/practice.ts
@@ -1,39 +1,22 @@
-interface Shape {
-    getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자입니다.
+interface Person {
+    name: string;
+    age?: number; // 물음표는 설정을 해도 되고 안해도 되고
 }
 
-// 상속받아 해당 클래스가 Shape interface 의 조건을 충족시킨다.
-class Circle implements Shape {
-    /* 생성자 */
-    constructor(public radius: number) {
-        this.radius = radius;
-    }
-
-    /* 오버라이드 함수 */
-    getArea() {
-        return this.radius * this.radius * Math.PI;
-    }
+interface Developer {
+    name: string;
+    age?: number;
+    skills: string[];
 }
 
-class Rectangle implements Shape {
-    constructor(private width: number, private height: number) {
-        this.width = width;
-        this.height = height;
-    }
+const person: Person = {
+    name: '김사람',
+    age: 20
+};
 
-    getArea() {
-        return this.width * this.height;
-    }
-}
+const expert: Developer = {
+    name: '김개발',
+    skills: ['javascript', 'react']
+};
 
-const circle = new Circle(5);
-const rectangle = new Rectangle(10, 5);
-
-console.log(circle.radius);
-console.log(rectangle.width); // private 이기에 에러 발생
-
-const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
-
-shapes.forEach(shape => {
-    console.log(shape.getArea());
-});
+const people: Person[] = [person, expert];
