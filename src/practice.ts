6a596cbaada9836d5db26009151b4ab3c7bc0a58
hwangbo <taekwonod714@naver.com>
2020-07-01 17:46:32 +0900

    feat: 인터페이스와 제네릭을 사용해보자

--- src/practice.ts
+++ src/practice.ts
@@ -1,16 +1,7 @@
-function merge<A, B>(a: A, b:B): A & B {
-    return {
-        ...a,
-        ...b
-    };
+interface Items<T> {
+    list: T[];
 }
 
-const merged = merge({ foo: 1 }, { bar: 1 });
-
-function wrap<T>(param: T) {
-    return {
-        param
-    }
-}
-
-const wrapped = wrap(10);
+const items: Items<string> = {
+    list: ['a', 'b', 'c']
+};
